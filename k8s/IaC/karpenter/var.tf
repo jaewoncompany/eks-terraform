3c1488f8e6cefa24e73a8ccffe838f0356198574
이기철 <s24049@gsm.hs.kr>
2025-03-17 23:36:41 +0900

    Add files via upload

---
+++ k8s/IaC/karpenter/var.tf
@@ -0,0 +1,20 @@
+variable "prefix" {
+  
+}
+
+variable "cluster_name" {
+  
+}
+
+variable "oidc_provider_arn" {
+  
+}
+
+variable "cluster_endpoint" {
+  
+}
+
+variable "node_group_id" {
+  
+}
+
